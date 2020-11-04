619f0c71f4c7d38a7029a90c723fa363fc25588c
jissy^C <jissyjose1989@gmail.com>
2020-11-04 11:25:28 +0530

    commandline runner

--- src/main/java/com/springboot/ticketbooking/TicketBookingManagementAppApplication.java
+++ src/main/java/com/springboot/ticketbooking/TicketBookingManagementAppApplication.java
@@ -1,13 +1,41 @@
 package com.springboot.ticketbooking;
 
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Date;
+import com.springboot.ticketbooking.entity.Ticket;
+import com.springboot.ticketbooking.service.TicketService;
 
 @SpringBootApplication
-public class TicketBookingManagementAppApplication {
+public class TicketBookingManagementAppApplication implements CommandLineRunner  {
+	@Autowired
+	private TicketService ticketService;
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TicketBookingManagementAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		
+		Ticket ticket=new Ticket();
+		ticket.setPassengerName("DUMMY");
+		ticket.setSourceStation("HYDERABAD");
+		ticket.setDestinationStation("CHENNAI");
+		ticket.setEmailAddress("dummy@gmail.com");
+		ticket.setTravelDate(new Date());
+		
+		ticketService.createTicket(ticket);
+
 	}
 
 }
